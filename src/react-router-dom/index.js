c329d1bf6b1c64dd50b71025e9ba44cae46066c1
weisuoke <530264127@qq.com>
2022-02-05 12:48:31 +0800

    feat: router 实现基本路由

--- src/react-router-dom/index.js
+++ src/react-router-dom/index.js
@@ -1,20 +1,65 @@
+import React from 'react'
+import { Router } from "../react-router";
+import { createBrowserHistory, createHashHistory } from "history";
+export * from '../react-router'
+
 /**
  * 一个 Router 用在浏览器端，提供最干净的URL
  * @param ref
  * @constructor
  */
-function BrowserRouter(ref) {
+function BrowserRouter({children}) {
+  let historyRef = React.useRef(null)
+  if (historyRef.current === null) {
+    historyRef.current = createBrowserHistory()
+  }
+  let history = historyRef.current;
+  let [state, setState] = React.useState({
+    action: history.action,
+    location: history.location
+  })
+  React.useLayoutEffect(() => {
+    history.listen(setState)
+  }, [history])
 
+  return (
+    <Router
+      children={children}
+      location={state.location}
+      navigator={history}
+      navigationType={state.action}
+    />
+  )
 }
 
 /**
  * 一个用在浏览器的 Router
  * 把路径保存在 URL 地址的 hash 部分，以便在改变的时候不会发送给服务器
- * @param ref
+ * @param children
  * @constructor
  */
-function HashRouter(ref) {
+function HashRouter({ children }) {
+  let historyRef = React.useRef(null)
+  if (historyRef.current === null) {
+    historyRef.current = createHashHistory()
+  }
+  let history = historyRef.current;
+  let [state, setState] = React.useState({
+    action: history.action,
+    location: history.location
+  })
+  React.useLayoutEffect(() => {
+    history.listen(setState)
+  }, [history])
 
+  return (
+    <Router
+      children={children}
+      location={state.location}
+      navigator={history}
+      navigationType={state.action}
+    />
+  )
 }
 
 export {
--- src/react-router/index.js
+++ src/react-router/index.js
@@ -1,9 +1,78 @@
-function Router(ref) {
+import React from 'react'
+const NavigationContext = React.createContext();
+const LocationContext = React.createContext();
+const RouteContext = React.createContext();
 
+/**
+ * 路由容器
+ * @param children 儿子
+ * @param navigator 历史对象，其实就是 history
+ * @param location 地址对象，{pathname: '地址路径'}
+ * @returns {JSX.Element}
+ * @constructor
+ */
+function Router({children, navigator, location}) {
+  return (
+    <NavigationContext.Provider value={{navigator}}>
+      <LocationContext.Provider value={{location}}>
+        {children}
+      </LocationContext.Provider>
+    </NavigationContext.Provider>
+  )
 }
 
-function Routes(ref) {
+function useLocation() {
+  return React.useContext(LocationContext).location
+}
 
+function Routes({ children }) {
+  return useRoutes(createRoutesFromChildren(children))
+}
+
+function useRoutes(routes) {
+  let location = useLocation()
+  let pathname = location.pathname || '/'
+  for (let i = 0; i < routes.length; i++) {
+    let { path, element } = routes[i];
+    let match = matchPath(path, pathname)
+    if (match) {
+      return element
+    }
+  }
+}
+
+/**
+ * 判断此route对应的path路径和地址中的pathname是否匹配
+ * @param path
+ * @param pathname
+ */
+function matchPath(path, pathname) {
+  let matcher = compilePath(path);
+  let match = pathname.match(matcher)
+  return match
+}
+
+/**
+ * 把路径转化成正则表达式
+ * @param path 路径
+ */
+function compilePath(path) {
+  let regexpSource = "^" + path;
+  regexpSource += '$';
+  let matcher = new RegExp(regexpSource)
+  return matcher
+}
+
+function createRoutesFromChildren(children) {
+  let routes = [];
+  React.Children.forEach(children, child => {
+    let route = {
+      path: child.props.path, // 代表 Route 的路径
+      element: child.props.element  // 代表此 Route 要渲染的元素
+    }
+    routes.push(route);
+  })
+  return routes
 }
 
 function Route(props) {
